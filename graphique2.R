3fff669407e1cd7c68345b05f1e1503d03e9425b
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2025-07-10 16:58:22 +0200

    update EE2024

--- graphique2.R
+++ graphique2.R
@@ -1,38 +1,36 @@
-rm(list = ls())
-setwd("~/Dropbox/ofce/2023-04-15-economie-europeenne/")
-source("../../code/R-markdown/init_eurostat.R")
 library(tidyverse)
 library(eurostat)
-library(countrycode)
-library(gt)
-library(gtExtras)
-load_data("eurostat/prc_hicp_midx.RData")
+geo_colors <- tribble(~ geo, ~ Geo, ~ color,
+                      "FR", "France", "#ED2939",
+                      "DE", "Allemagne", "#000000",
+                      "EA19", "Zone Euro", "#003399",
+                      "ES", "Espagne", "#FFC400",
+                      "IT", "Italie", "#009246")
 
-load_data("flags/flagColors_data.RData")
-load_data("flags/colors.RData")
+## Load Eurostat datasets ------
 
-load_data("eurostat/lc_lci_r2_q.RData")
-names <- lc_lci_r2_q %>%
-  select_if(~ n_distinct(.) > 1) %>%
-  names(.)
+datasets_eurostat <- c("prc_hicp_midx", "lc_lci_r2_q")
 
-for (variable in names[c(-length(names), -length(names)+1)]){
-  load_data(paste0("eurostat/", variable, ".RData"))
+for (dataset in datasets_eurostat){
+  assign(dataset, 
+         get_eurostat(dataset, stringsAsFactors = F, cache = F) |>
+           rename(time = TIME_PERIOD)
+  )
 }
+
+## Graphique 2 --------
 
 lc_lci_r2_q_extract <- lc_lci_r2_q %>%
   filter(nace_r2 == "B-S",
          unit == "I16",
          s_adj == "SCA",
          lcstruct == "D11",
-         geo %in% c("FR", "DE", "IT", "ES", "EA"))%>%
-  left_join(geo, by = "geo") %>%
-  select(geo, Geo, time, values) %>%
-  quarter_to_date %>%
+         geo %in% c("FR", "DE", "IT", "ES", "EA")) %>%
+  select(geo, date = time, values) %>%
   # + 3 months
   mutate(date = date + months(3)) %>%
   filter(date >= as.Date("2017-01-01")) %>%
-  group_by(Geo) %>%
+  group_by(geo) %>%
   arrange(date) %>%
   mutate(wages = 100*values/values[1]) %>%
   select(-values)
@@ -41,17 +39,15 @@ prc_hicp_midx_extract <- prc_hicp_midx %>%
   filter(unit == "I15",
          coicop %in% c("CP00"),
          geo %in% c("FR", "DE", "IT", "ES", "EA")) %>%
-  left_join(geo, by = "geo") %>%
-  select(geo, Geo, time, values) %>%
-  month_to_date %>%
+  select(geo, date = time, values) %>%
   filter(date >= as.Date("2017-01-01")) %>%
-  group_by(Geo) %>%
+  group_by(geo) %>%
   arrange(date) %>%
   mutate(prices = 100*values/values[1]) %>%
   select(-values)
 
 graphique2 <- lc_lci_r2_q_extract %>%
-  left_join(prc_hicp_midx_extract, by = c("date", "geo", "Geo")) %>%
+  left_join(prc_hicp_midx_extract, by = c("date", "geo")) %>%
   ungroup %>%
   transmute(date, geo, real_wages = 100*wages/prices)
 
@@ -64,6 +60,8 @@ geo_colors <- tribble(~ geo, ~ Geo, ~ color,
 
 
 graphique2 %>%
+  filter(date >= as.Date("2017-01-01"),
+         date <= as.Date("2023-04-01")) %>%
   left_join(geo_colors, by = "geo") %>%
   ggplot(.) + geom_line(aes(x = date, y = real_wages, color = Geo, linetype = Geo), size = 1) +
   theme_minimal() + xlab("") + ylab("Salaires réels (100 = Janvier 2017)") +
